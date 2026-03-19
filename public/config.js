18339481d14457b33ba4eabff2efcd0cd2d2b106
Mario Reder <mario.reder@pm.me>
2026-03-19 11:05:18 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Markets,Portfolio,Vaults,Swap,Rewards,Points",
-  "VITE_CUSTOM_MENUS": "Home, https://www.paridex.xyz",
+  "VITE_CUSTOM_MENUS": "Home, https://www.parlidex.xyz",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
